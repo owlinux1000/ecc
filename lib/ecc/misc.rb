853d7a30056f976a4a99b3ae3f786fccd15abbaa
owlinux1000 <pgm3rdlinuxor1000@gmail.com>
2017-10-15 17:25:00 +0900

    ADD baby-step giant-step method

--- lib/ecc/misc.rb
+++ lib/ecc/misc.rb
@@ -1,7 +1,28 @@
 module Ecc
 
   # Baby-step Giant-step method
-  def self.baby_step_giant_step()
+  def self.bsgs(curve, p, q)
+    
+    m = Math.sqrt(curve.point_order(p)).to_i
+    
+    baby = []
+    b = q
+    
+    m.times do
+      baby << b
+      b -= p
+    end
+
+    giant = p * m
+    
+    1.step(m).each do |i|
+      return i*m + baby.index(giant) if baby.include?(giant)
+      giant += (p * m)
+    end
+    
+    puts "Not found d"
+    
+    nil
     
   end
   
